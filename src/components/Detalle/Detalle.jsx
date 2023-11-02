45ba14ec6650e5ff658eca6270e741a4bb8b388a
lucacarena97 <lucacarena98@gmail.com>
2023-11-02 19:33:00 -0300

    taildwind

--- src/components/Detalle/Detalle.jsx
+++ src/components/Detalle/Detalle.jsx
@@ -1,47 +1,47 @@
 import Descripcion from "../descripcion/Descripcion";
-import detalleStyle from "./Detalle.module.css";
+// import detalleStyle from "./Detalle.module.css";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Detalle() {
   return (
     <>
-      <section className={detalleStyle.detalles}>
+      <section className="flex gap-40 m-10 justify-center mr-40">
         <div>
           <img
-            className={detalleStyle.imagenVista}
+            className=""
             src="/src/images/Detalle_producto/buzo.jpg"
             alt="buzo"
           />
         </div>
-        <div className={detalleStyle.info}>
-          <p className={detalleStyle.texto}>
+        <div className="mt-10 w-1/4">
+          <p className="text-xl w-4/5">
             Sweater de hombre en mezcla de alpaca con detalles de la marca y
             cocodrilo a contraste
           </p>
-          <div className={detalleStyle.price}>
-            <p className={detalleStyle.precio}>$74.000</p>
-            <p className={detalleStyle.promo}>Promos bancarias</p>
+          <div className="mt-8 flex justify-between px-8">
+            <p className="font-bold">$74.000</p>
+            <p className="">Promos bancarias</p>
           </div>
-          <div className={detalleStyle.color}>
+          <div className="my-9 flex items-center gap-10 px-4">
             <img
-              className={detalleStyle.imagenDetalle}
+              className="w-16"
               src="/src/images/Detalle_producto/color.avif"
               alt="buzo"
             />
             <p>Color Seleccionado</p>
           </div>
           <hr />
-          <div className={detalleStyle.talla}>
-            <p className={detalleStyle.select}>Seleccionar talla</p>
-            <MdKeyboardArrowRight className={detalleStyle.flecha} />
+          <div className="flex justify-between items-center my-6">
+            <p className="">Seleccionar talla</p>
+            <MdKeyboardArrowRight className="text-2xl" />
           </div>
           <hr />
-          <div className={detalleStyle.boton}>
-            <button className={detalleStyle.botonCarrito}>
+          <div className="my-8 text-center">
+            <button className="bg-green-700 p-4 text-white w-2/4">
               Agregar al carrito
             </button>
           </div>
-          <div className={detalleStyle.envio}>
+          <div className="">
             <p>Envio Estándar - $ Precio</p>
             <p>Devolucion gratis</p>
           </div>
