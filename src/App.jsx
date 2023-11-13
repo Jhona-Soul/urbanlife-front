4e132faec9c20d8ad9e8fb36a5a567c0964bd814
lucacarena97 <lucacarena98@gmail.com>
2023-11-13 19:19:26 -0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 //Dependencias
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 //Components
-import { Layout } from './components/Layout';
+import { Layout } from "./components/Layout";
 import Footer from "./components/Footer/Footer";
 import Detalle from "./components/Detalle/Detalle";
-import Card from "./components/ProductCard/Card";
-
+// import Card from "./components/ProductCard/Card";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout/>} >
-          <Route path="detalle" element={<Detalle/>} />
-
+        <Route path="/" element={<Layout />}>
+          <Route path="detalle" element={<Detalle />} />
         </Route>
-        
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
